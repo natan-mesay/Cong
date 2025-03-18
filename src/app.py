df6da024cbad3880bb3a8aaeb86468062ed931f2
natan-mesay <natanmesay2000@gmail.com>
2025-03-18 08:53:54 +0300

    major changes on the codebase

--- src/app.py
+++ src/app.py
@@ -1,54 +1,18 @@
-from fastapi import FastAPI, HTTPException, Request
-from telegram import Update
-from telegram.ext import Application
+from fastapi import FastAPI, Request, BackgroundTasks
+from src.bot.bot import updater, bot, TOKEN
 from dotenv import load_dotenv
-import os
-from src.bot.handlers import register_handlers
-from contextlib import asynccontextmanager
-import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Load environment variables
-load_dotenv()
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
-# Initialize FastAPI app
 app = FastAPI()
 
-# Initialize Telegram bot application
-application = Application.builder().token(TOKEN).build()
+@app.on_event("startup")
+async def startup_event():
+    updater.start_polling()
+
+@app.post("/webhook")
+async def webhook(update: dict, background_tasks: BackgroundTasks):
+    updater.dispatcher.process_update(update)
+    return "ok"
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-# Webhook endpoint
-@app.post("/webhook")
-async def webhook(request: Request):
-    try:
-        data = await request.json()
-        logger.info(f"Incoming update: {data}")  # Log the incoming update
-
-        update = Update.de_json(data, application.bot)
-        await application.process_update(update)
-        return {"status": "ok"}
-    except Exception as e:
-        logger.error(f"Error processing update: {e}", exc_info=True)
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-        
-
-# Set webhook URL on startup
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    url = "https://cong-acuj.onrender.com"  # Replace with your Render URL
-    await application.bot.set_webhook(url)
-    register_handlers(application)
-    yield
-    
-    await application.bot.delete_webhook()
-
-# Run the app
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+async def read_root():
+    return {"message": "Hello, this is the FastAPI Chatbot server."}
